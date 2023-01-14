037ccbdc416fda387d7c3fa9166116f302d9d0ee
Alexey4717 <lehabourne@gmail.com>
2023-01-15 05:48:43 +1000

    change format returned errors

--- src/middlewares/input-validations-middleware.ts
+++ src/middlewares/input-validations-middleware.ts
@@ -5,9 +5,15 @@ import {HTTP_STATUSES} from '../types';
 
 
 export const inputValidationsMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    const errors = validationResult(req);
+    const errorFormatter = ({ location, msg, param, value, nestedErrors }: any) => {
+        return {
+            message: msg,
+            field: param
+        };
+    };
+    const errors = validationResult(req).formatWith(errorFormatter);
     if (!errors.isEmpty()) {
-        res.status(HTTP_STATUSES.BAD_REQUEST_400).json({errors: errors.array()});
+        return res.status(HTTP_STATUSES.BAD_REQUEST_400).json({errors: errors.array({ onlyFirstError: true })});
     } else {
         next();
     }
