52dc8ff623318be010613fb89444187d215a5fbd
Alexey4717 <lehabourne@gmail.com>
2025-05-03 18:41:09 +1000

    add styles for active tab

--- apps/web/src/components/layout/sidebar/menus/SidebarMenu.tsx
+++ apps/web/src/components/layout/sidebar/menus/SidebarMenu.tsx
@@ -1,24 +1,31 @@
+import { match } from 'path-to-regexp';
+import { usePathname } from 'next/navigation';
 import type { ISidebarItem } from '../sidebar.types';
 import { MenuItem } from './MenuItem';
 
 interface Props {
-	title?: string;
-	menu: ISidebarItem[];
+    title?: string;
+    menu: ISidebarItem[];
 }
 
 export function SidebarMenu({ menu, title }: Props) {
-	return (
-		<nav>
-			{title && (
-				<div className="opacity-40 uppercase font-medium text-xs mb-3">
-					{title}
-				</div>
-			)}
-			<ul>
-				{menu.map((menuItem) => (
-					<MenuItem key={menuItem.label} item={menuItem} />
-				))}
-			</ul>
-		</nav>
-	);
+    const pathname = usePathname();
+    return (
+        <nav>
+            {title && (
+                <div className="opacity-40 uppercase font-medium text-xs mb-3">
+                    {title}
+                </div>
+            )}
+            <ul>
+                {menu.map((menuItem) => (
+                    <MenuItem
+                        key={menuItem.label}
+                        item={menuItem}
+                        isActive={!!match(menuItem.link)(pathname)}
+                    />
+                ))}
+            </ul>
+        </nav>
+    );
 }
